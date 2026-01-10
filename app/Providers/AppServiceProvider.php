d7e83447e74b2971cb41bc3d20b4e098a5f61fc4
pariiee <alghifariee8@gmail.com>
2026-01-10 22:44:12 +0700

    deploy githu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,27 +7,31 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
+use App\Console\Commands\MakeDeployScript;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        // DAFTAR COMMAND ARTISAN (LARAVEL 11)
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                MakeDeployScript::class,
+            ]);
+        }
     }
 
     public function boot(): void
     {
         /**
-         * Rate limit register (punya kamu, TETAP)
+         * Rate limit register
          */
         RateLimiter::for('register-ip', function (Request $request) {
-            return Limit::perMinutes(2880, 3)
-                ->by($request->ip());
+            return Limit::perMinutes(2880, 3)->by($request->ip());
         });
 
         /**
-         * FLAG GLOBAL DEVTOOLS PROTECTION
-         * cukup 1 kali set di sini
+         * Global flag devtools protection
          */
         View::share('devtoolsProtection', true);
     }
